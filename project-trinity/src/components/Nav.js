21f392bbae34877e9785e2a82ce644c972461b14
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-08-05 13:13:39 -0700

    Updated nav bar

--- project-trinity/src/components/Nav.js
+++ project-trinity/src/components/Nav.js
@@ -61,11 +61,15 @@ useEffect(() => {
   return (
     <nav className="navbar">
       <div className="navbar-emblem">
-        <Link className='landing-page-link' to="/home"> <img  id="emblem" src={logo} alt="emblem of trinity la"/></Link>
+        <Link className='landing-page-link' to="/home"><p className='logo'>RAFAEL CARRILLO</p></Link>
+        <div className="left-menu">
+          <Link id='landing-page-link' className="hover-underline" to="/about"> about </Link>
+          <Link id='landing-page-link' className="hover-underline" to="/about"> work </Link>
+        </div>
         <motion.div 
         id="menu"
         initial={{ height: 0, opacity: 0 }}
-        animate={menuOpen ? { height: '100%', opacity: 1, top: '100px' } : { height: 0, opacity: 0 }}
+        animate={menuOpen ? { height: '100%', opacity: 1, top: '50px' } : { height: 0, opacity: 0 }}
         transition={{ duration: .3 }}
         style={menuOpen ? { position: 'fixed', backgroundColor: '#070F19', top: '0'}: {display: 'none'}}>
         <div className='flourescent-nav'></div>
@@ -75,26 +79,20 @@ useEffect(() => {
           animate={menuOpen ? { y: 0, opacity:  1 }: {y: '3vw', opacity: 0} } // Animate opacity to 1
           transition={{ duration: 1 }}
           className="hamburger-menu">
-        {/* <li><Link className='landing-page-link' to="/portfolio"> Portfolio </Link></li>
-        <li><Link className='landing-page-link' to="/resume"> Resume </Link></li> */}
-          {/* <li><Link className='landing-page-link' to="/shop"> Shop </Link></li> */}
-          {/* <li><Link className='landing-page-link' to="/blog"> Blog </Link></li> */}
-          {/* <li><Link className='landing-page-link' to="/collections"> Collections </Link></li> */}
-          <li><Link className='landing-page-link' to="/about"> About </Link></li> 
-          <li><Link className='landing-page-link' to="/contact"> Contact </Link></li>
+       
+          <li><Link className='landing-page-link' to="/about"> about </Link></li> 
+          <li><Link className='landing-page-link' to="/contact"> work </Link></li>
+          <li><Link className='landing-page-link' to="/contact"> contact </Link></li>
+          <li className='location'>los angeles, ca</li> 
           
         </motion.ul>
       </motion.div>
       </div>
       <div className="nav-menu">
-        {/* <Link id='landing-page-link' className="hover-underline" to="/portfolio"> Portfolio </Link> */}
-        {/* <Link id='landing-page-link' className="hover-underline" to="/portfolio"> Resume </Link> */}
-        {/* <Link id='landing-page-link' className="hover-underline" to="/shop"> Shop </Link> */}
-        {/* <Link id='landing-page-link' className="hover-underline" to="/blog"> Blog </Link> */}
-        {/* <Link id='landing-page-link' className="hover-underline" to="/collections"> Collections </Link> */}
-        <Link id='landing-page-link' className="hover-underline" to="/about"> About </Link>
-        <Link id='landing-page-link' className="hover-underline" to="/contact"> Contact </Link>
-
+        <div className='nav-right-container'>
+        <Link id='landing-page-link' className="hover-underline" to="/contact"> contact </Link>
+        <li><Link className='landing-page-link'>los angeles, ca </Link></li> 
+        </div>
       </div>
       <div className='menu-container'>
         <div>
@@ -102,14 +100,14 @@ useEffect(() => {
             <motion.ul 
             className="hamburger">
               <motion.li 
-                animate={{  y: menuOpen ? 2: 0, x: menuOpen ? 10: 0, rotate: menuOpen ? 60: 0}}
+                animate={{  y: menuOpen ? 2: 0, x: menuOpen ? 1: 0, rotate: menuOpen ? 45: 0, scale: menuOpen ? .7: 1}}
                 transition={{ duration: 0 }}></motion.li>
               <motion.li 
-                animate={{y: menuOpen ? -7: 0, x: menuOpen ? -10: 0, rotate: menuOpen ? -60: 0}}
+                animate={{y: menuOpen ? -7: 0, x: menuOpen ? -0: 0, rotate: menuOpen ? -45: 0, scale: menuOpen ? .7: 1}}
                 transition={{ duration: 0 }}></motion.li>
-                <motion.li 
+                {/* <motion.li 
                 animate={{y: menuOpen ? 1: 0, rotate: menuOpen ? 0: 0}}
-                transition={{ duration: 0 }}></motion.li>
+                transition={{ duration: 0 }}></motion.li> */}
               
             </motion.ul>
           </div>
