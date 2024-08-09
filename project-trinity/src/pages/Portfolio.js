f0dacba502bcca8e08d0b9905f9afd5540881f96
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-08-09 15:40:45 -0700

    removed trinity assets

--- project-trinity/src/pages/Portfolio.js
+++ project-trinity/src/pages/Portfolio.js
@@ -10,7 +10,6 @@ import Nav from '../components/Nav';
 
 // Assets
 import hero from '../assets/hero-shot.jpeg'
-import stars from '../assets/stars.svg'
 import Footer from '../components/Footer';
 import { motion } from 'framer-motion';
 import logo from '../assets/logo.svg'
