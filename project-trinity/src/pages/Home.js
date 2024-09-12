990106282c5fea7a917c866d1c24dd7f419985c2
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-09-11 22:21:53 -0700

    updated introduction page

--- project-trinity/src/pages/Home.js
+++ project-trinity/src/pages/Home.js
@@ -15,6 +15,9 @@ import SpotifyNow from '../components/SpotifyNow';
 import ScrollingText from '../components/scrollingText';
 import ScrollToTop from '../components/ScrollToTop';
 import LoadingScreen from '../components/LoadingScreen';
+import instagram from '../assets/icons/instagram.svg'
+import linkedIn from '../assets/icons/linkedIn.svg'
+import github from '../assets/icons/github.svg'
 
 import React, { useEffect, useRef } from 'react';
 
@@ -76,8 +79,59 @@ function Home() {
         <p className='header'>CURRENTLY AVAILABLE FOR CRAFTING EXPERIENCES</p>
       </section>  
 
-      
-      <Carousel/>
+      <section className='introduction-page'>
+      <div id="vine-three" >
+        <dotlottie-player 
+          
+          src="https://lottie.host/c9f78807-0c1f-411c-b164-5a82b3bae9ed/ZXcOSYPY8t.json"
+          background="transparent" 
+          speed=".5" 
+          style={{width: 400, height: 400}} 
+          autoPlay
+          ref={lottiePlayerRef}>
+        </dotlottie-player>
+      </div>
+
+      <div id="vine-four" >
+        <dotlottie-player 
+          
+          src="https://lottie.host/c9f78807-0c1f-411c-b164-5a82b3bae9ed/ZXcOSYPY8t.json"
+          background="transparent" 
+          speed=".5" 
+          style={{width: 400, height: 400, transform: 'scaleX(-1) scaleY(-1)', }} 
+          autoPlay
+          ref={lottiePlayerRef}>
+        </dotlottie-player>
+      </div>
+        <div className='introduction-text'>
+        <h1>
+          Hi! I’m Rafael. 
+        </h1>
+        <h2 className='gray-text'>
+          I love <span className='green-font'>🎨 design</span>, <span className='green-font'>💻 technology</span>, <br/> and <span className='green-font'>solving problems </span>utilizing a <br/> human-centered process. 🌎
+        </h2>
+        <h3>
+        EDUCATION
+        </h3>
+        <h2 className='school'>
+          University of California, Irvine - 2024
+        </h2>
+        <h3 id="indent" className='gray-text'>
+          Bachelor of Science in Informatics <br/>
+          Minor in Informations and Computer Science <br/>
+          Specialization in Human-Computer Interaction <br/>
+        </h3>
+        <h3>
+        FIND ME
+        </h3>
+        <div id="indent" className='icons-container'>
+          <img src={instagram} alt="instagram" />
+          <img src={linkedIn} alt="LinkedIn" />
+          <img src={github} alt="GitHub" />
+        </div>
+        </div>
+        <Carousel/>
+      </section>
       <div className='offer-container'>
         <div className='offer1'>
           <h1>
