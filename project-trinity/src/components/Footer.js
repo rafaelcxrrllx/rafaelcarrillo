29877c09d90a026ec127952922e03af8b488ea90
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-10-01 22:07:38 -0700

    Added Connection to Footer

--- project-trinity/src/components/Footer.js
+++ project-trinity/src/components/Footer.js
@@ -5,28 +5,101 @@ import '../styling/styles.css'
 import '../styling/Footer.css'
 
 // Assets
-import linkedin from '../assets/icons/linkedIn.svg';
+import instagram from '../assets/icons/instagram.svg'
+import linkedIn from '../assets/icons/linkedIn.svg'
+import github from '../assets/icons/github.svg'
+import { motion } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
+import VinesFooter from '../components/VinesFooter';
 
 
 
 function Footer() {
 
+  const lottiePlayerRefFooter = useRef(null);
+  const vinePlayerRefFooter = useRef(null);
+
+
+  useEffect(() => {
+    const lottieElementFooter = lottiePlayerRefFooter.current;
+    const vineElementFooter = vinePlayerRefFooter.current;
+
+    const handlePlayAnimation = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          lottieElementFooter.play();
+          vineElementFooter.play();
+        } else {
+          lottieElementFooter.stop();
+          vineElementFooter.stop();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handlePlayAnimation, {
+      threshold: 0.5, // Trigger when 50% of the element is visible
+    });
+
+    if (lottieElementFooter) {
+      observer.observe(lottieElementFooter);
+    }
+    if (vineElementFooter) {
+      observer.observe(vineElementFooter);
+    }
+
+    return () => {
+      if (lottieElementFooter) {
+        observer.unobserve(lottieElementFooter);
+      }
+      if (vineElementFooter) {
+        observer.unobserve(vineElementFooter);
+      }
+    };
+  }, []);
+
   return (
 
-    <footer>
-      <div className='footer-container'>
-     
+    <motion.footer
+    initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+    animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+    transition={{ duration: 2, delay: 1.5 }}>
+      <section className="footer-section">
+      {/* <div id="vine-one" >
+        <dotlottie-player 
+          
+          src="https://lottie.host/c9f78807-0c1f-411c-b164-5a82b3bae9ed/ZXcOSYPY8t.json"
+          background="transparent" 
+          speed=".5" 
+          style={{width: 400, height: 400}} 
+          autoPlay
+          ref={vinePlayerRefFooter}
+          >
+        </dotlottie-player>
+      </div>
 
-          {/* <div>
-              <a href='https://www.linkedin.com/in/rafaelcxrrllx/'><img src={linkedin} alt="instagram logo"/></a>
-          </div> */}
+      <VinesFooter/> */}
+
+     
+       <h1 className='connect-title'>
+          Let's Connect!
+        </h1>
+         <div className='footer-icons-container'>
+         <a class="hot-links" href="https://www.instagram.com/rafaelcxrrllx/"><img src={instagram} alt="instagram" /> </a> 
+          <a class="hot-links" href="https://www.linkedin.com/in/rafaelcxrrllx/"><img src={linkedIn} alt="LinkedIn" /> </a>
+          <a class="hot-links" href="https://github.com/rafaelcxrrllx"><img src={github} alt="GitHub" /> </a>
+        </div>
+      <h3 className='green-font' id="quote">
+        “Work is love made visible.” - Khalil Gibran
+      </h3>
+      <div className='footer-container'>
           <div>
               <p className='logo'>
               2024 © RAFAEL CARRILLO
               </p>
           </div>
       </div>
-    </footer>
+      </section>
+    </motion.footer>
   );
 }
 
---
+++ project-trinity/src/components/VinesFooter.js
@@ -0,0 +1,69 @@
+
+// Global Styling
+import '../styling/styles.css'
+
+// Local Styling
+import '../styling/Home.css'
+
+
+
+import React, { useEffect, useRef } from 'react';
+
+
+
+function VinesFooter() {
+  // "homepage": "https://rafaelcxrrllx.github.io/trinityla",
+  const lottiePlayerRefFooter = useRef(null);
+
+  useEffect(() => {
+    const lottieElement = lottiePlayerRefFooter.current;
+
+    const handlePlayAnimation = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          lottieElement.play();
+        } else {
+          lottieElement.stop();
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handlePlayAnimation, {
+      threshold: 0.5, // Trigger when 50% of the element is visible
+    });
+
+    if (lottieElement) {
+      observer.observe(lottieElement);
+    }
+
+    return () => {
+      if (lottieElement) {
+        observer.unobserve(lottieElement);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <section>
+
+      <div id="vine-two" >
+        <dotlottie-player 
+          
+          src="https://lottie.host/c9f78807-0c1f-411c-b164-5a82b3bae9ed/ZXcOSYPY8t.json"
+          background="transparent" 
+          speed=".5" 
+          style={{width: 400, height: 400, transform: 'scaleX(-1) scaleY(-1)', }} 
+          autoPlay
+          ref={lottiePlayerRefFooter}
+          >
+        </dotlottie-player>
+      </div>
+
+      </section>  
+       
+    </div>
+  );
+}
+
+export default VinesFooter;
